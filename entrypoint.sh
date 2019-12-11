8b6f4c043b10bad3da48890856d6928ae0f0266b
liyu <liyu@redhat.com>
2019-12-11 14:40:41 +0800

    added jenkins pipeline file for CI build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ "$#" == "4" ]; then
-    sed -i s/"<thread>"/"$2"/g ./inputs/properties/container.properties
-    sed -i s/"<url>"/"$3"/g ./inputs/properties/container.properties
-    sed -i s/"<port>"/"$4"/g ./inputs/properties/container.properties
-    jmeter -n -t ./tests/$1 -S ./inputs/properties/container.properties -l ./$(basename $1).log
+    sed -i s/"<thread>"/"$2"/g /src/inputs/properties/container.properties
+    sed -i s/"<url>"/"$3"/g /src/inputs/properties/container.properties
+    sed -i s/"<port>"/"$4"/g /src/inputs/properties/container.properties
+    jmeter -n -t /src/tests/$1 -S /src/inputs/properties/container.properties -l /src/$(basename $1).log
 elif [ "$#" == "2" ]; then
-    jmeter -n -t ./tests/$1 -S ./inputs/properties/$2 -l ./$(basename $1).log
+    jmeter -n -t /src/tests/$1 -S /src/inputs/properties/$2 -l /src/$(basename $1).log
 else
     echo "invalid number of arguments"
 fi
