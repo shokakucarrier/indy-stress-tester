cba820a59d5b577885dab69c19e2505f435b57c4
liyu <liyu@redhat.com>
2019-12-17 15:33:53 +0800

    added cache performance benchmark script, enhanced start process

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ DIR=`dirname ${SCRIPT}`
 #INDY_WORKDIR=${INDY_WORKDIR:-$PWD}
 
 JMETER=${JMETER:-$HOME/apache-jmeter-5.1.1/bin/jmeter}
-TEST=${JMETER_TEST:-$DIR/tests/upload-simulation-existing.jmx}
+TEST=${JMETER_TEST:-$DIR/tests/download-simulation-existing.jmx}
 LOG=${JMETER_LOG:-"$DIR/$(basename $TEST).log"}
 PROPS=${JMETER_PROPS:-$DIR/inputs/properties/local.properties}
 
@@ -18,15 +18,20 @@ echo "Starting Indy..."
 docker run --rm --name indy_local -d -p 8080:8080 -p 8081:8081 quay.io/factory2/indy:latest-release
 
 echo "Waiting for Indy startup to complete..."
-while ! lsof -i -P | grep 8080 | grep LISTEN 2>&1 > /dev/null; do
-  #ps ax | grep $INDY_PID | grep -v grep 2>&1 > /dev/null
-  if [ $? -ne 0 ]; then
-  	echo "Indy did not start properly."
-  	exit 2
-  fi
-
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
+#while ! lsof -i -P | grep 8080 | grep LISTEN 2>&1 > /dev/null; do
+#  #ps ax | grep $INDY_PID | grep -v grep 2>&1 > /dev/null
+#  if [ $? -ne 0 ]; then
+#  	echo "Indy did not start properly."
+#  	exit 2
+#  fi
+#
+#  sleep 0.1 # wait for 1/10 of the second before check again
+#done
+while ! nc -z localhost 8080;
+do
+  echo sleeping;
+  sleep 1;
+done;
 
 
 echo "$JMETER -n -t $TEST -S $PROPS -l $LOG"
