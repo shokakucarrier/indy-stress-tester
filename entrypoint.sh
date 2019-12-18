f1b239aa855ea39bed69531d42f97c1be9b89f42
liyu <liyu@redhat.com>
2019-12-18 18:18:33 +0800

    init support DA stress test script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-if [ "$#" == "1" ]; then
+if [ "$1" == "da-simulation-existing.jmx" ]; then
+    sed -i "s/<dahostname>/$DA_HOSTNAME/g" /src/inputs/properties/da.properties
+    sed -i "s/<git_repo_name>/$GIT_REPO_NAME/g" /src/inputs/properties/da.properties
+    mkdir -p /usr/share/git
+    git clone $GIT_REPO_URL /usr/share/git/$GIT_REPO_NAME
+elif [ "$#" == "1" ]; then
     sed -i "s/<thread>/$THREAD/g" /src/inputs/properties/container.properties
     sed -i "s/<url>/$HOSTNAME/g" /src/inputs/properties/container.properties
     sed -i "s/<port>/$PORT/g" /src/inputs/properties/container.properties
